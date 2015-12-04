e3e176a8a87dcc70ee0402bfbe6ed27e251fe94f
grand <grand@Patrice-Bureau>
2015-12-04 16:07:11 +0100

    modification du nom des variables

--- ajax/traiterconnexion.php
+++ ajax/traiterconnexion.php
@@ -6,8 +6,8 @@ $login = $_REQUEST['login'];
 
 $user = verifuser($login, $mdp);
 if($user!="")
- $_SESSION['id'] = $user;  
-echo $user;
+ $_SESSION['user'] = $user;  
+echo json_encode($user);
 
 
 ?>
