381e362695b214a5120d81a5dfbc8c90dae264fa
patricegrand <patricegrand@free.fr>
2015-12-06 23:25:16 +0100

    Update traitergerermesoffres.php

--- ajax/traitergerermesoffres.php
+++ ajax/traitergerermesoffres.php
@@ -1,12 +1,13 @@
 <?php
-require_once '../util/fonctions.php';
+require_once '../util/class.pdocovoiturage.inc.php';
 session_start();
-$id = $_SESSION['user']['id'];;
+$id = $_SESSION['user']['id'];
+$pdo=PdoCovoiturage::getPdo();
 $toutesMesOffres = array();
 $toutesMesOffres['departlycee']= array();
-$toutesMesOffres['departlycee'] = getMesOffresDepartLycee($id);
+$toutesMesOffres['departlycee'] = $pdo->getMesOffresDepartLycee($id);
 $toutesMesOffres['arriveelycee']= array();
-$toutesMesOffres['arriveelycee']= getMesOffresArriveeLycee($id);
+$toutesMesOffres['arriveelycee']= $pdo->getMesOffresArriveeLycee($id);
 /* A faire*/
 // $toutesMesOffres = getMesOffres($id) : retourne toutes les offres d'un chauffeur
 echo json_encode($toutesMesOffres);
