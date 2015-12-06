d2fdf94f09703f283de82cd876ab63c10b269961
patricegrand <patricegrand@free.fr>
2015-12-06 22:16:05 +0100

    Update traiterconnexion.php

--- ajax/traiterconnexion.php
+++ ajax/traiterconnexion.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-require_once '../util/fonctions.php';
+require_once '../util/class.pdocovoiturage.inc.php';
 $mdp = $_REQUEST['mdp'];
 $login = $_REQUEST['login'];
 // A faire
